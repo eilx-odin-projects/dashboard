e8233a12a104c056815471af15d37e5d44ebed1b
eilx <eil23@pm.me>
2022-05-26 18:44:12 +0100

    Complete implementation

--- main.js
+++ main.js
@@ -1,3 +1,33 @@
-const $ = document.querySelector.bind(document)
+customElements.define('x-trend', class extends HTMLAnchorElement {
+    constructor () {
+        super()
 
-feather.replace()
+        this.innerHTML = `
+            <img width="50" height="50" class="circle" src="${this.dataset.img}"/>
+            <span>
+                <strong>@${this.dataset.name}</strong>
+                <p>${this.dataset.desc}</p>
+            </span>
+        `
+    }
+}, { extends: 'a' })
+
+customElements.define('x-announcement', class extends HTMLAnchorElement {
+    constructor () {
+        super()
+
+        this.innerHTML = `
+            <h3>${this.dataset.title}</h3>
+            <p>${this.dataset.desc}</h3>
+        `
+    }
+}, { extends: 'a' })
+
+customElements.define('x-project', class extends HTMLElement {
+    constructor () {
+        super()
+
+        const element = document.getElementById('ProjectCard').content.cloneNode(true)
+        this.attachShadow({ mode: "open" }).appendChild(element)
+    }
+})
